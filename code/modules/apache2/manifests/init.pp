f6207d83af81f1c53e01aa30757fd0536140cd0f
Ricardo Williams <infinent97@gmail.com>
2019-07-21 08:56:07 -0700

    puppet naming scheme?

--- code/modules/apache2/manifests/init.pp
+++ code/modules/apache2/manifests/init.pp
@@ -18,7 +18,7 @@ class apache2 {
 		mode    => '444',
 		owner   => 'root',
 		group   => 'root',
-		source  => "puppet:///code/modules/html",
+		source  => "puppet:///modules/html/html",
 		require => Package["apache2"],
 	}
 
