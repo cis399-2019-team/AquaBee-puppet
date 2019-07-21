2afcadae939bd766e2e1bf7e67ef898177425daa
Ricardo Williams <infinent97@gmail.com>
2019-07-21 07:57:18 -0700

    trying the recurse=>true

--- code/modules/apache2/manifests/init.pp
+++ code/modules/apache2/manifests/init.pp
@@ -12,6 +12,16 @@ class apache2 {
 		require => Package["apache2"],
 	}
 
+	file { "/var/www/html":
+		ensure  => directory,
+		recurse => true,
+		mode    => '444',
+		owner   => 'root',
+		group   => 'root',
+		source  => "puppet:///modules/code/html",
+		require => Package["apache2"],
+	}
+
 	service { "apache2":
 		enable    => true,
 		ensure    => running,
