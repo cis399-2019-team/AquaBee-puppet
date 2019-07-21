a543d7cc954fb0aed01a425f85a45a3cc41889c2
Ricardo Williams <infinent97@gmail.com>
2019-07-21 08:52:48 -0700

    retrying code/modules/html

--- code/modules/apache2/manifests/init.pp
+++ code/modules/apache2/manifests/init.pp
@@ -18,7 +18,7 @@ class apache2 {
 		mode    => '444',
 		owner   => 'root',
 		group   => 'root',
-		source  => "puppet:///modules/html",
+		source  => "puppet:///code/modules/html",
 		require => Package["apache2"],
 	}
 
