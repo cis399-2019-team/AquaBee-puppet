7a034d79e23373a089511a4f802536d69d5b4776
Ricardo Williams <infinent97@gmail.com>
2019-07-21 08:17:55 -0700

    this works? inside of code?

--- code/modules/apache2/manifests/init.pp
+++ code/modules/apache2/manifests/init.pp
@@ -18,7 +18,7 @@ class apache2 {
 		mode    => '444',
 		owner   => 'root',
 		group   => 'root',
-		source  => "puppet:///modules/code/html",
+		source  => "puppet:///html",
 		require => Package["apache2"],
 	}
 
