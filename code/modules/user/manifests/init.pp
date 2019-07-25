f7c57619d2a844b66420ba0e2e0874f0917451fa
Ricardo Williams <infinent97@gmail.com>
2019-07-25 15:21:45 -0700

    just kidding here we go

--- code/modules/user/manifests/init.pp
+++ code/modules/user/manifests/init.pp
@@ -62,7 +62,7 @@ class user{
 
 	ssh_authorized_key{"stevev_rsa":
 	user => "stevev",
-	type => "ssh",
+	type => "ssh-rsa",
 	key => "AAAAC3NzaC1lZDI1NTE5AAAAILBL3wWzbtQXl6V3saF13p5TM1mdhyrYQZYOqTLw2RT8"
 	}
 
