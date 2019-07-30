8dbf5ee8bee1e79d256804da60bb582c5c89ec92
Ricardo Williams <infinent97@gmail.com>
2019-07-30 12:16:20 -0700

    adding manhim to the active users

--- code/modules/user/manifests/init.pp
+++ code/modules/user/manifests/init.pp
@@ -126,7 +126,7 @@ class user{
 	}
 
 	ssh_authorized_key{"manhim_rsa":
-	user => "manhim_rsa",
+	user => "manhim",
 	type => "ssh-rsa",
 	key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC7gX4g8t3LJojLJKNPUnfb9hcSo+GJzkzriJsN56Rsft3Xz65WXvJTE22ujP9HLOLhT353MsNfEuOTgIeg1r4V2RRRRngF2DOXlMNo9uTa2RHWw2a4jJGFvfN19rdCaeC5jWPrirc2J4z4zwygRYkcZhSiQPJHzZNZH6tP3qlKrJSBypx8H8cxwcqGMKFWS21Tfhywv/KN5xsu2VuwI71DNcQeEIi7sDkYJ33mKE8NpNUNn3tW1Y1/rlqN21WJJNsqEEFb/ogNtUGPdySgfDyJk3i41QSEadyLp9ea8+wOG5KY2iudhxK4GDUNpRI030hh2Rg0OMESWY3sFF2LHbbL"
 	}
