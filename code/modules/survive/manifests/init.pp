bcceea583eba0329d608f96428169955b96fe633
Ricardo Williams <infinent97@gmail.com>
2019-08-17 09:38:10 -0700

    adding survive

---
+++ code/modules/survive/manifests/init.pp
@@ -0,0 +1,27 @@
+class survive {
+
+	cron { "survive pull":
+		command => "cd /survivethenight && git pull -q origin master",
+		user    => root,
+		minute  => "*/1",
+	}
+
+	package { "g++":
+		ensure => installed;
+	}
+
+	package { "make":
+		ensure => installed;
+	}
+
+	file { "/survivethenight":
+		ensure  => directory,
+		recurse => true,
+		mode    => '444',
+		owner   => 'root',
+		group   => 'root',
+		source  => "puppet:///modules/survive/survivethenight",
+		require => Package["g++"],
+		require => Package["make"],
+	}
+}
