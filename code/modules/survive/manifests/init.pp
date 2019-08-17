7ba751008d4d18d0fd135d0a66b6bd84c0c765d6
Ricardo Williams <infinent97@gmail.com>
2019-08-17 12:10:14 -0700

    adding more things

--- code/modules/survive/manifests/init.pp
+++ code/modules/survive/manifests/init.pp
@@ -15,123 +15,74 @@ class survive {
 	}
 
 	vcsrepo { "/home/ubuntu/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "ubuntu",
 	}
 
 	vcsrepo { "/home/austin/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "austin",
 	}
 
 	vcsrepo { "/home/devin/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "devin",
 	}
 
 	vcsrepo { "/home/henzi/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "henzi",
 	}
 
 	vcsrepo { "/home/james/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "james",
 	}
 
 	vcsrepo { "/home/jerry/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "jerry",
 	}
 
 	vcsrepo { "/home/keir/survivethenight":
-		ensure  => directory,
-		recurse => true,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "keir",
 	}
 
 	vcsrepo { "/home/lucas/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "lucas",
 	}
 
 	vcsrepo { "/home/manhim/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "manhim",
 	}
 
 	vcsrepo { "/home/mapu/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "mapu",
 	}
 
 	vcsrepo { "/home/owen/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "owen",
 	}
 
 	vcsrepo { "/home/stevev/survivethenight":
-		ensure  => directory,
-		mode    => '444',
-		owner   => 'root',
-		group   => 'root',
+		ensure  => present,
 		provider => git,
 		source  => "https://bitbucket.org/discretelando/survivethenight",
-		user => "stevev",
 	}
 }
